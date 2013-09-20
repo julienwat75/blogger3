425dcca34f18d33694ceb6b38c9440479d73fb35
julienwat75 <julien_wat@hotmail.fr>
2013-09-20 16:00:16 +0200

    mise en place form offre

--- app/controllers/offres_controller.rb
+++ app/controllers/offres_controller.rb
@@ -17,5 +17,28 @@ def show
 end
 
 
+def new
+  @offres = Offre.new
+  
+end
+
+def create              # le submit va chercher la methode create
+  
+
+  @offres = Offre.new(profil_params)
+  @offres.titre = params[:offre][:titre]  # on reccupere le nom du form
+  @offres.description = params[:offre][:description] # on reccupere le body du form 
+  @offres.profil_id ="8"
+  @offres.save   #on sauvegarde
+   redirect_to offres_path     # redirection vers l'index
+
+end
+
+def profil_params
+    params.require(:offre).permit(:titre, :description)
+  end
+
+
+
 
 end
